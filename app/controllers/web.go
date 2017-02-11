300339ad8f046ec79fb0c3818bfd97dfb08d7fea
M G <msgrasser@gmail.com>
2017-02-11 12:59:06 -0500

    fixed bugs in parsing new bool

--- app/controllers/web.go
+++ app/controllers/web.go
@@ -82,8 +82,14 @@ func (c Web) FirstTimeSetupAction(org_title, org_subtitle, org_mbtiles_file, org
 		return c.Redirect(Web.LoginForm)
 	}
 
+	// convert enc to bool
+	org_enc_is_on, err := strconv.ParseBool(org_enc_is_on)
+	if err != nil {
+		panic(err)
+	}
+
 	// create deployment
-	if createDeployment(org_title, org_subtitle, org_mbtiles_file, org_map_center_lat, org_map_center_lon, org_map_zoom_min, org_map_zoom_max, strconv.ParseBool(org_enc_is_on)) {
+	if createDeployment(org_title, org_subtitle, org_mbtiles_file, org_map_center_lat, org_map_center_lon, org_map_zoom_min, org_map_zoom_max, org_enc_is_on) {
 
 		// create new key for organization
 		skek := cryptoWrapper.RandString(32)
